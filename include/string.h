77ee25d989ba80f4dfbbb543ec4f79a6215bdb8a
0xVarun <vtewari26@gmail.com>
2019-09-12 23:18:58 +0530

    an attempt to build an application without using glibc implemented syscalls: 	write 	exit implemented library functions: 	strlen

---
+++ include/string.h
@@ -0,0 +1,13 @@
+#ifndef __STRING_H
+#define __STRING_H
+
+/**
+ * @brief string length
+ * @details Returns the String length
+ * 
+ * @param r string
+ * @return length
+ */
+int strlen(char *);
+
+#endif // __STRING_H
